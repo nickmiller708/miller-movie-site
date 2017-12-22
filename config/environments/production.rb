7e9d4f1efedb80fb5ac5227b5561bf38dc5e31cb
Nick Miller <nickmiller708@gmail.com>
2017-12-22 05:46:49 +0000

    spelling error. I hate myself

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.assets.compile = false
   
    config.action_mailer.default_url_options = { :host => "miller-movie-site.herokuapp.com" } 
-config.action_mailer.delivery_method = :smpt
+config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
