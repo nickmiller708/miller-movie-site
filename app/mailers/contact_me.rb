7b5f14a1879dab592a48e5082a51740d8795eeff
Nick Miller <nickmiller708@gmail.com>
2017-12-22 05:49:55 +0000

    Adding sender copy subject

--- app/mailers/contact_me.rb
+++ app/mailers/contact_me.rb
@@ -4,13 +4,13 @@ class ContactMe < ApplicationMailer
     @sender = params[:send_name] || "Name not given :("
     @sender_email = params[:contact_email]
     @message = params[:message]
-    mail(to: 'nickmiller708@gmail.com', from: 'miller-time-mailer-bot@miller.com', subject: "New Message from #{@sender}")  
+    mail(to: @sender_email, from: 'millerhonestreview@gmail.com', subject: "New Message from #{@sender}")
   end
 
   def sender_copy(params)
     @sender = params[:send_name] || "Name not given :("
     @sender_email = params[:contact_email]
     @message = params[:message]
-    mail(to: 'nickmiller708@gmail.com', from: 'miller-time-mailer-bot@miller.com', subject: "New Message from #{@sender}")
+    mail(to: @sender_email, from: 'millerhonestreview@gmail.com', subject: "Sender Copy: New Message from #{@sender}")
   end  
 end
