8290be7146b3c5366510783f56a790d04ba2495c
Nick Miller <nickmiller708@gmail.com>
2017-12-27 04:50:26 +0000

    Finally... think will work nice on mobile

--- app/helpers/review_posts_helper.rb
+++ app/helpers/review_posts_helper.rb
@@ -1,5 +1,5 @@
 module ReviewPostsHelper
   def convert_watch_url(relevant_url)
-    YouTubeAddy.youtube_embed_url(relevant_url, '100%', '5%')
+    YouTubeAddy.youtube_embed_url(relevant_url, '100%', 235)
   end 
 end
