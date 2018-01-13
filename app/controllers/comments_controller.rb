f4aa1b76c38f865157bc20283c79525c04ebbe92
Nick Miller <nickmiller708@gmail.com>
2018-01-13 06:34:14 +0000

    Hopefully fixing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -70,5 +70,5 @@ class CommentsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
       params.require(:comment).permit(:name, :comment, :review_post_id)    
-end
+    end
 end
